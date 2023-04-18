8e6e52e9cb4f00c1d98e37df7c505f40100f6c6b
Frilaa <113754630+Frilaa@users.noreply.github.com>
2023-04-18 19:55:38 +0700

    Add files via upload

---
+++ Instagram/src/ConstData.js
@@ -0,0 +1,14 @@
+const UserObj = [
+    { id: 1, name: "Frila", username: "frilacahyawrdn", img: require("../assets/images/foto.jpeg"), gender: "F", is_view: true, cap: "anjayy"},
+    { id: 2, name: "Adrian", username: "adrian_adhari", img: require("../assets/images/mobil.jpg"), gender: "M", is_view: true },
+    { id: 3, name: "Kevina", username: "acililul", img: require("../assets/images/turu.jpeg"), gender: "F", is_view: true, cap: "turu dek"},
+    { id: 4, name: "Subhan", username: "kevinn", img: require("../assets/images/kucing.jpg"), gender: "M", is_view: true, cap: "Jimmy" },
+    { id: 5, name: "Hera", username: "heradwi", img: require("../assets/images/trio.jpeg"), gender: "F", is_view: true, cap: "Senggol Dong" },
+    { id: 6, name: "Salma", username: "salmaraisya", gender: "F", is_view: true },
+    { id: 7, name: "Teddy", username: "uwa_eddy", gender: "M", is_view: false },
+    { id: 8, name: "Daphne", username: "daphne_holy", gender: "F", is_view: false },
+    { id: 9, name: "Ferdy", username: "ferdyapriliyanto", gender: "M", is_view: false },
+    { id: 10, name: "Febri", username: "febrydfairuz", gender: "M", is_view: false }
+];
+
+export { UserObj };
